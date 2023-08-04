69bd59b1fe293ad66da3af8e4256b5b4e51aaabd
Riazantsev <riazantsevmark327@gmail.com>
2023-08-04 20:34:17 +0400

    units of measurement

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double height = 1.87;
+        System.out.println("Рост: " + height + " м ");
         int weight = 98;
+        System.out.println("Вес: " + height + " кг ");
         int bmi = service.calculate(weight, height);
         System.out.println("Body mass index: " + bmi);
     }
